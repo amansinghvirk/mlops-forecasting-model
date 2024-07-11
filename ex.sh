ab1078682802a9aaaffe63cd1a89f2ecaea13415
AmandeepSinghEVS <amandeep.singh2@evalueserve.com>
2024-07-11 08:57:13 -0400

    deleted: ex.sh deleted to remove duplicacy!

--- ex.sh
+++
@@ -1,7 +0,0 @@
-python -m venv .venv
-source .venv/Scripts/activate
-pip install -r requirements.txt
-python mlmodel/src/train.py --execution_name "Validation Sets" --experiments_list "/c/mydata/work/mlproject/final_project/models/model_type_a_stores/params/Validation Sets.yaml"
-python mlmodel/src/train.py --execution_name "Variables Set" --experiments_list "/c/mydata/work/mlproject/final_project/models/model_type_a_stores/params/Variables Set.yaml"
-cd mlmodel/src
-uvicorn experiment_api:app --port $2 --reload
